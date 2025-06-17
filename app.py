35c6fd260a8e524e26e481bdb27710944d4a8faa
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-06-17 18:30:33 +0700

    feat: add page for detection
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return 'Hello, World! From home!'
+    year = datetime.now().year
+    return render_template("index.html", year=year)
 
-@app.route('/detection')
+@app.route('/detection', methods=['POST'])
 def detection():
-    return 'Hello, World! From detection!'
+    class_names = [
+        "Burger",
+        "Donat",
+        "Hot dog",
+        "Pizza",
+        "Sandwich",
+        "Baked Potato",
+        "Crispy Chicken",
+        "Fries",
+        "Taco",
+        "Taquito",
+    ]
+
+    confidences = [random.randint(1, 100) for _ in class_names]
+    total_confidence = sum(confidences)
+    normalized_confidences = [round((c / total_confidence) * 100, 2) for c in confidences]
+
+    data = [
+        {"name": class_name, "confidence": confidence}
+        for class_name, confidence in zip(class_names, normalized_confidences)
+    ]
+
+    data.sort(key=lambda x: x["confidence"], reverse=True)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
