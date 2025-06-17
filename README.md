bb73958280ede44402ab955531e08b59b27c71dd
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-06-17 20:03:55 +0700

    feat: add image detection implementation and update readme.md
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Fast Food Detection
+# Fast Food Detection (Resnet50)
+
+- Dataset: https://www.kaggle.com/datasets/utkarshsaxenadn/fast-food-classification-dataset
+- Collab: https://colab.research.google.com/drive/1XJRFf7IaIq6Huw72nRhvR_oP3-Vfjtws?usp=sharing
+- Models (best_f1_model.pth): https://drive.google.com/drive/folders/1y7Qn1yQTK9STAzclyxJ1sft0qTwlEOdY?usp=sharing
+
+### Created by:
+- Muhamad Rizal Arfiyan (22.11.5227)
+- Gilang Nur Hidayat (22.11.5196)
+- Damar Galih (22.11.5209)
--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from flask import Flask, render_template, jsonify
 from datetime import datetime
-import random
+import io
+from flask import Flask, request, jsonify
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from PIL import Image
 
 app = Flask(__name__)
+class_names = ['Baked Potato', 'Burger', 'Crispy Chicken', 'Donut', 'Fries', 'Hot Dog', 'Pizza', 'Sandwich', 'Taco', 'Taquito']
+
+model = models.resnet50(weights=None)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, len(class_names))
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.load_state_dict(torch.load('models/model.pth', map_location=device))
+model.to(device)
+model.eval()
+
+preprocess = transforms.Compose([
+    transforms.Resize(226),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+])
+
+def transform_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    return preprocess(image).unsqueeze(0)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -11,30 +36,35 @@ def index():
 
 @app.route('/detection', methods=['POST'])
 def detection():
-    class_names = [
-        "Burger",
-        "Donat",
-        "Hot dog",
-        "Pizza",
-        "Sandwich",
-        "Baked Potato",
-        "Crispy Chicken",
-        "Fries",
-        "Taco",
-        "Taquito",
-    ]
+    if 'image' not in request.files:
+        return jsonify({"error": "File tidak ditemukan"}), 400
+    
+    image = request.files['image']
+    
+    if image.filename == '':
+        return jsonify({"error": "Tidak ada image yang dipilih"}), 400
 
-    confidences = [random.randint(1, 100) for _ in class_names]
-    total_confidence = sum(confidences)
-    normalized_confidences = [round((c / total_confidence) * 100, 2) for c in confidences]
+    try:
+        img_bytes = image.read()
+        tensor = transform_image(img_bytes)
+        tensor = tensor.to(device)
 
-    data = [
-        {"name": class_name, "confidence": confidence}
-        for class_name, confidence in zip(class_names, normalized_confidences)
-    ]
+        with torch.no_grad():
+            outputs = model(tensor)
+            probabilities = torch.nn.functional.softmax(outputs[0], dim=0)
+        
+        results = []
+        for i, prob in enumerate(probabilities):
+            confidence = float(prob.item()) * 100
+            class_name = class_names[i]
+            results.append({"name": class_name, "confidence": confidence})
 
-    data.sort(key=lambda x: x["confidence"], reverse=True)
-    return jsonify(data)
+        results.sort(key=lambda x: x['confidence'], reverse=True)
+        return jsonify(results)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
